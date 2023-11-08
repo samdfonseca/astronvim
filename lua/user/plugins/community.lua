3d25d45adf292432d3ef249224c8ea2581017f84
Sam Fonseca <samfonseca@utexas.edu>
2023-11-07 21:32:58 -0500

    initial commit

---
+++ lua/user/plugins/community.lua
@@ -0,0 +1,25 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.clojure" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.zig" },
+
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
+
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.flash-nvim" },
+
+  { import = "astrocommunity.debugging.telescope-dap-nvim"},
+}
